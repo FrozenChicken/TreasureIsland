8eca4fd191f3168f5e9c3ea35e492c9d6ff1c7a2
Aleksander <78713400+FrozenChicken@users.noreply.github.com>
2021-08-13 22:15:57 +0200

    Add files via upload

---
+++ Treasure island.py
@@ -0,0 +1,234 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+options= input("Your open your eyes. You're on unknow ship...\n The only thing you're thinking right now is terryfing sore temple. What will you do ?\n That's big bundle your hand's are not tied\n There're many options but u have to choice only one from few..( just write first, second, third, or fourth) \n The first one is to ask beautiful girl about what's going on\n the second one is to jump off throw the railing\n the third one is to just wait what happen \n and the last one is to go under deck\n\n")
+
+
+
+if options == "first" or options =="FIRST" or options == "First": 
+  print('''    ,-\';__.
+          ./ ( _\)_\\
+         //  )(e (e( )
+        ( ` (    > //
+       __\__/' __=/`\
+      /'    ` ~/ )   )
+     |  _      ,(_,_/
+     /  /\  o )   o)\
+    /  , \` -     -  \___
+ __/  /___\       |\____, \____________
+ ''''')
+  print("Oh shit, u start remember all those things.. She's your ex-girlfriend, you remember all this awfull things, she's addicted to drugs, she cheated on you, she stole your puppie and broke your heart, right now she's sniffing you like she sniffs coke, ahhhh this paintful, you're feeling like you are changing in atoms and valence electrons that you remember from the chemistry lesson..")
+  option1_1 = input("Wanna wake up ? or wanna die ? just chooice one ( wake up or die)\n\n")
+  if option1_1== "die" or option1_1=="Die" or option1_1=="DIE":
+    print(""'''------____              ________________________________
+          \_         __/    ___---------__
+            \      _/      /              \_
+             \    /       /                 \
+              |  /       | _    _ \          \
+              | |       / / \  / \ |          \
+              | |       ||   ||   ||           |
+              | |       | \_//\\_/ |           |
+              | |       |_| (||)   |_______|   |
+              | |         |  ||     | _  / /   |
+               \ \        |_________|| \/ /   /
+                \ \_       |_|_|_|_|/|  _/___/
+                 \__>       _ _/_ _ /  |
+                          .|_|_|_|_|   |
+                          |           /
+                          |__________/''')
+    print("you are soft like our generation of people just died, no one will remember about you expect this whore")
+  else:
+    print('''     .---------.
+       _    |:: [-=-] |
+      | |   |_________|
+      |~|
+      |_|                    ,;;;;,
+       I\  ,__ ,;;;, __,    ///\\\\\
+       I |{   / . . \   }   / "  \\||
+       I | ) (   _   ) (    \_= _///
+       I |{___'-. .-'___}\___ )_\
+       I ||~/,'~~~~~,\~~|'---((  \
+       I \ //        \\ |     \ \ \
+       I  \/         // |     | /-/
+       I (/         (/  |     |/||\
+       I  |             |     |    |
+       I  |             |     |____/
+       I  :-----_o_-----:      || |
+       I  | /~~|===|~~\ |      (( |
+       I  ||   |===|   ||      ||_/
+      /^\ "~   '^^^'   ""     ((__|
+''')
+    print("you open your eyes again, very bright light blinds yourself, you see her, your honey and the another person is the doctor.. He is saying this words' Stop taking heroine this should kill you!'You woke up in wrong place that's not the case about this little game\n you lost:c ")
+    print('¯\_(ツ)_/¯')
+elif options=="second" or options=="SECOND" or options=="Second":
+  print("Are you retarded? you are in the middle of the ocean and wanna jump off hahah\n Your lost")
+
+  print('''~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+         .               `         /
+                          .    ,../...       .
+          .                .  /       `\  /  .
+     \    .        o         < '  )     =<
+     /\  .                    \ \      /  \   .  __
+   >=)'>                       `'\'"'"'         /o \/
+     \/ .    /         o              /,        \__/\    .:/
+     /   .  /--\ /         /         <')=<     .      ,,///;,   ,;/
+           ::::::::;;\\\
+             \            \_/            .            ''\\\\\'' ';\
+    (                      \              .   __
+     )                                       <'_><          (
+    (          (                ,/..          `              )
+     )     (    )             <')   `=<                )    (
+    (       )  (               ``\```                 (      )
+_____)_____(____)______________________________________)____(_______''''')
+elif options=="third" or options=="THIRD" or options=="Third":
+  print("""      ,-'-.     _.,  
+        . (    '("'-'  ').  
+     ( ' ((  |.      )\/( ) 
+      '(  )) | () |" |  | ')
+         ( . ,-. ,-.. __.) 
+           /)  /  ' /         
+          /   /) / /    ""\n\n\n""")
+  op2_2 = input("a heavy rain begins. Lightning bolts streak across the sky. One of them hits the ship. The ship begins to burn. What are you doing ? You are helping opponents or you waiting for the end?(type help or wait)\n")
+
+  if op2_2 == "Help" or op2_2 == "HELP" or op2_2 == "help":
+    print("""     *
+    _|           __
+   (__  Question  _)
+                 |
+                 *
+""""")
+    a= input("You notice the old fluffy pirate who get stucked into ropes which were falling down from the most and you see amazing women who falls down into abbys that leads to parallel world. Who' ll stay alive? That's one choice ( print women or pirate)\n ")
+    if a == "pirate" or a == "Pirate" or a =="PIRATE":
+       print("""  _____
+              .-" .-. "-.
+            _/ '=(0.0)=' \_
+          /`   .='|m|'=.   `\
+          \________________ /
+      .--.__///`'-,__~\\\\~`
+     / /6|__\// a (__)-\\\\
+     \ \/--`((   ._\   ,)))
+     /  \\  ))\  -==-  (O)(
+    /    )\((((\   .  /)))))
+   /  _.' /  __(`~~~~`)__
+  //"\\,-'-"`   `~~~~\\~~`"-.
+ //  /`"              `      `\
+//""""""""")
+    elif a== "Women" or a== "women" or a== "WOMEN":
+      print("""           _____|\
+                  _.--| SSt |:
+                 <____|.----||
+                        .---''---,
+                         ;..__..'    _...
+                       ,'/  ;|/..--''    \
+                     ,'_/.-/':            :
+                _..-'''/  /  |  \    \   _|/|
+               \      /-./_ \;   \    \,;'   \
+               ,\    / \:  `:\    \   //    `:`.
+             ,'  \  /-._;   | :    : ::    ,.   .
+           ,'     ::   /`-._| |    | || ' :  `.`.)
+        _,'       |;._:: |  | |    | `|   :    `'
+      ,'   `.     /   |`-:_ ; |    |  |  : \
+      `--.   )   /|-._:    :          |   \ \
+         /  /   :_|   ;`-._;   __..--';    : :
+        /  (    ;|;-./_  _/.-:'o |   /     ' |
+       /  , \._/_/_./--''/_|:|___|_,'        |
+      :  /   `'-'--'----'---------'          |
+      | :     O ._O   O_. O ._O   O_.      ; ;
+      : `.      //    //    //    //     ,' /
+    ~~~`.______//____//____//____//_______,'~
+              //    //~   //    //
+       ~~   _//   _//   _// ~ _//     ~
+     ~     / /   / /   / /   / /  ~      ~~
+          ~~~   ~~~   ~~~   ~~~""")
+      b=input("You jumped to the her, but unfortunetlly you forgot about the very important fact,you never had been in the gym.. You can not hold her. What will you do? Fall with her and fell amazing feeling about being loved for a few second or just stay on the burning ship? ( type 'love' or 'stay )'")
+    else:
+      print=("""  _  __________=__
+        \\@([____]_____()
+       _/\|-[____]
+      /     /(( )
+     /____|'----'
+     \____/      """)
+      print("this stupid faggot killed you after your help.. \n\n you lost")
+    if b=="Love" or b=="love" or b=="love":
+      print("""
+                          *  *       *  * 
+                        *      *   *      *
+                        *       * *       *
+                         *       *       *   
+                           *           *
+                             *       *
+                               *   *
+                                * *             love
+                                 *
+""")
+      print("you feel tremendous fear. You are going to die for the idea for the love for the people. Suddenly you come in void with her feeling resemble jump in to the water on the pool. oh shit here we go again.. you wake up again, again... It's dark and cramped, you think it's a coffin. You lift the lid with your foot. you woke up in a chest in an alternative world.. U can smell her body next to you, you're starting understand all, you be right, you choosen greatest option you choosen future mom of your childrens. She informed you about being bill gates doughter she's rich, stonks\n\n The End\n\n\n you win")
+  else:
+   print("""        (  .      )
+           )           (              )
+                 .  '   .   '  .  '  .
+        (    , )       (.   )  (   ',    )
+         .' ) ( . )    ,  ( ,     )   ( .
+      ). , ( .   (  ) ( , ')  .' (  ,    )
+     (_,) . ), ) _) _,')  (, ) '. )  ,. (' )
+ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^""")
+   print(" Right now you are like chicken wings after 4 hours in the roaster, you're burnt\n\n you lost ")
+else: 
+  print("you dead like real man like captain, but u wasn't the captain so you stupid, you fell down from the stairs and you crashed your head\n\n you lost")
+  print(" ¯\_(ツ)_/¯\n¯\_(ツ)_/¯\n¯\_(ツ)_/¯\n")
+  print("""  ___-----------___
+           __--~~                 ~~--__
+       _-~~                             ~~-_
+    _-~                                     ~-_
+   /                                           \
+  |                                             |
+ |                                               |
+ |                                               |
+|                                                 |
+|                                                 |
+|                                                 |
+ |                                               |
+ |  |    _-------_               _-------_    |  |
+ |  |  /~         ~\           /~         ~\  |  |
+  ||  |             |         |             |  ||
+  || |               |       |               | ||
+  || |              |         |              | ||
+  |   \_           /           \           _/   |
+ |      ~~--_____-~    /~V~\    ~-_____--~~      |
+ |                    |     |                    |
+|                    |       |                    |
+|                    |  /^\  |                    |
+ |                    ~~   ~~                    |
+  \_         _                       _         _/
+    ~--____-~ ~\                   /~ ~-____--~
+         \     /\                 /\     /
+          \    | ( ,           , ) |    /
+           |   | (~(__(  |  )__)~) |   |
+            |   \/ (  (~~|~~)  ) \/   |
+             |   |  [ [  |  ] ]  /   |
+              |                     |
+               \                   /
+                ~-_             _-~
+                   ~--___-___--~
+""")
